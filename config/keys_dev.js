02f5e48803c1f9c715cd8a6f2e7d27ea65a2ab9f
anshumansinha18 <sinhaanshuman18298@gmail.com>
2021-08-02 12:08:51 +0530

    First Commit

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports =
+  "mongodb+srv://admin:7fx6wiDzAGOOm3ZH@icinema-4p2wg.mongodb.net/iCinema?retryWrites=true";
+
+// mongodb+srv://admin:7fx6wiDzAGOOm3ZH@icinema-4p2wg.mongodb.net/iCinema?retryWrites=true
